5a272caca19be1f8ab11b0c7e8eb02c1bb1aa5e2
Aveek Majumdar <aveek.majumdar123456@gmail.com>
2020-11-09 20:54:46 +0530

    ..

---
+++ book-master-service/src/main/java/microservices/test/bookmasterservice/BookMasterServiceApplication.java
@@ -0,0 +1,13 @@
+package microservices.test.bookmasterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMasterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMasterServiceApplication.class, args);
+	}
+
+}
---
+++ book-master-service/src/test/java/microservices/test/bookmasterservice/BookMasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.test.bookmasterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMasterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
